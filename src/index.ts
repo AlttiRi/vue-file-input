f0d7fadbf003ee998f21c5e9f23fc77e958bf25e
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2024-08-19 16:28:39 +0300

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 export {default as FileInput}                 from "./components/FileInput.vue";
+export {default as FileInputDefault}          from "./components/FileInputDefault.vue";
 export {default as FileInputDefaultText}      from "./components/FileInputDefaultText.vue";
 export {default as FileInputDefaultHoverText} from "./components/FileInputDefaultHoverText.vue";
 export {default as FileInputSelectedInfo}     from "./components/FileInputSelectedInfo.vue";
 
 // Deprecated. Use `FileInputSelectedInfo`.
-export {default as FileInputFileInfo} from "./components/FileInputSelectedInfo.vue";
+export {default as FileInputFileInfo}         from "./components/FileInputSelectedInfo.vue";
 
 
 export {WebFileEntry}     from "./components/WebFileEntry.ts";
