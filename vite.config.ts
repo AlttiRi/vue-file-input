f5f56d10fd8bba5712f68a1539ef4edc6fbb6c1c
AlttiRi <16310547+AlttiRi@users.noreply.github.com>
2026-06-23 01:14:17 +0300

    Bump deps

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import {defineConfig} from "vite";
+import vuePlugin from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [
+    vuePlugin(),
+  ],
+  base: "./",
+  server: {
+    open: "./"
+  },
+  esbuild: {
+    target: "es2021",
+  },
+  build: {
+    target: "es2020",
+    sourcemap: true,
+    minify: true,
+    rollupOptions: {
+      output: {
+        minifyInternalExports: false,
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+        manualChunks: (id) => {
+          if (id.includes("@vue"))     { return "vue";  }
+          if (id.includes("@alttiri")) { return "util"; }
+        },
+      }
+    }
+  }
+});
